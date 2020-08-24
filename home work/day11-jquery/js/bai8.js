58ecb9c318bdfdaf7e3f1417d6b3e48dddda77b3
mickey112020 <giomuadong090@gmail.com>
2020-08-25 00:01:42 +0700

    day11-jquery dropdow menu

--- home work/day11-jquery/js/bai8.js
+++ home work/day11-jquery/js/bai8.js
@@ -1,16 +1,25 @@
 $(document).ready(function(){
-	$('#menu li:hover').mouseover(function(){
-		// $('#menu li:hover .sub-menu').attr('display', 'block');
-		alert('the li dc hover');
+	$('#menu  ul > li').mouseover(function(){
+		// $('#menu > ul > li > a:hover .sub-menu').attr('display', 'block');
+		// alert('the li dc hover');
 		// var obj_test = $(this).attr('display');
 		// console.log (obj_test);
+		$('#id-jquery').html('noi dung h3 chih sua bang ham mouseover');
+		// $('#sub-menu').attr('display','block');
+		// console.log(obj_test);
+		$(this).children('ul').attr('display','block');
+		var abc = $(this).children('ul').attr('display');
+		console.log(abc);
 
 // 	});
 // 	$('#id-a').attr('href', '#');
 // 	// console.log(obj_id);
 // 	// console.log('ban dang trong ham jsquery');
-
-// 	$('#id-jquery').mouseover(function(){
-// 		$(this).html('noi dung h3 chinh sua boi jquery');
 	});
+	// var obj_li = $('.sub-menu li a').attr('href');
+	// console.log(obj_li);
+	// $('#id-jquery').mouseover(function(){
+		// $(this).html('noi dung h3 chinh sua boi jquery');
+	// });
 });
+
