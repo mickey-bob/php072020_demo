e57609f744706cbd74f0294cf3122fbdfe401e60
Nguyen Trong Khanh <giomuadong090@gmail.com>
2020-08-24 17:21:28 +0700

    dropdown memu jsquery day11-js

---
+++ home work/day11-jquery/js/bai8.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('#menu li:hover').mouseover(function(){
+		// $('#menu li:hover .sub-menu').attr('display', 'block');
+		alert('the li dc hover');
+		// var obj_test = $(this).attr('display');
+		// console.log (obj_test);
+
+// 	});
+// 	$('#id-a').attr('href', '#');
+// 	// console.log(obj_id);
+// 	// console.log('ban dang trong ham jsquery');
+
+// 	$('#id-jquery').mouseover(function(){
+// 		$(this).html('noi dung h3 chinh sua boi jquery');
+	});
+});
