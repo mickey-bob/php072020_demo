a568f6d2ec1799cfffea6e52ff4fe37c863f9be2
Nguyen Trong Khanh <giomuadong090@gmail.com>
2020-08-25 18:14:47 +0700

    done dropdownmeu jquery, bai8day10

---
+++ Codes_demo/js/demo-jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('#div-id > ul > li').click(function(){
+		$('#id-h3').attr('style','display: inline');
+		$('#id-a').attr('href','https//google.com');
+	});
+});
--- home work/day11-jquery/js/bai8.js
+++ home work/day11-jquery/js/bai8.js
@@ -1,20 +1,18 @@
 $(document).ready(function(){
-	$('#menu  ul > li').mouseover(function(){
-		// $('#menu > ul > li > a:hover .sub-menu').attr('display', 'block');
+	$('#menu > ul > li').mouseover(function(){
 		// alert('the li dc hover');
-		// var obj_test = $(this).attr('display');
-		// console.log (obj_test);
-		$('#id-jquery').html('noi dung h3 chih sua bang ham mouseover');
-		// $('#sub-menu').attr('display','block');
+		$(this).find('.sub-menu').attr('style', 'display:block');
+		// $('#menu > ul > li > a:hover .sub-menu').attr('display', 'block');
+		// $(this).find('a').attr('href','https://dantri.com.vn/');
 		// console.log(obj_test);
-		$(this).children('ul').attr('display','block');
-		var abc = $(this).children('ul').attr('display');
-		console.log(abc);
 
 // 	});
 // 	$('#id-a').attr('href', '#');
 // 	// console.log(obj_id);
 // 	// console.log('ban dang trong ham jsquery');
+	});
+	$('#menu > ul > li').mouseout(function(){
+		$(this).find('ul.sub-menu').attr('style', 'display:none');
 	});
 	// var obj_li = $('.sub-menu li a').attr('href');
 	// console.log(obj_li);
