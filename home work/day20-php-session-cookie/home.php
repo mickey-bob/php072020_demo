98962a28ca369853222988d1547f83ad036d083e
mickey112020 <giomuadong090@gmail.com>
2020-10-04 23:52:30 +0700

    home work login.php day 20

--- home work/day20-php-session-cookie/home.php
+++ home work/day20-php-session-cookie/home.php
@@ -5,3 +5,12 @@
  * Date: 10/2/2020
  * Time: 2:08 PM
  */
+session_start();
+echo "<pre>";
+print_r($_SESSION);
+echo "</pre>";
+
+?>
+<h3><?php echo $_SESSION['success'];?></h3>
+<h3>Xin chao <?php echo $_SESSION['fullname'];?></h3>
+<a href="logout.php">logout</a>
--- home work/day20-php-session-cookie/login.php
+++ home work/day20-php-session-cookie/login.php
@@ -6,6 +6,7 @@
  * Time: 2:08 PM
  */
 //debug $_post
+session_start();
 echo "<pre>";
 print_r($_POST);
 echo "</pre>";
@@ -23,6 +24,25 @@ if (isset($_POST['submit_login'])){
     }elseif (strlen($password) <= 6){
         $error = "password ngan qua ban oi, toi thieu 6 character nhe";
     }
+
+    if ($error == ''){
+        if (isset($_POST['checkbox'])){
+            setcookie(name_cookie, $fullname, time()+600);
+            setcookie(pass_cookie, md5($password), time()+600);
+        }
+        $fullname = $_POST['username'];
+        $password = $_POST['password'];
+        $_SESSION['fullname'] = $fullname;
+        $_SESSION['success'] = 'ban da dang nhap thanh cong';
+        header('location: home.php');
+        exit();
+    }
+}
+if (isset($_COOKIE['name_cookie']) && isset($_COOKIE['pass_cookie'])){
+    $_SESSION['fullname'] = $_COOKIE['name_cookie'];
+    $_SESSION['success'] = "Ban da auto login thanh cong";
+    header("location: home.php");
+    exit();
 }
 
 ?>
@@ -32,7 +52,8 @@ if (isset($_POST['submit_login'])){
     <br />
     password: <input type="password" name="password" value="" />
     <br />
+    ghi nho dang nhap: <input type="checkbox" name="checkbox" value="1" />
     <input type="submit" name="submit_login" value="kick de login" />
 </form>
-<h3 style="color: red"></h3>
-<h3 style="color: blue"></h3>
+<h3 style="color: red"><?php echo "$error"; ?></h3>
+<h3 style="color: blue"><?php echo "$result"; ?></h3>
